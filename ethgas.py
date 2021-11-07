3ace72bb8208f86494186363acfbf1d0f8e11ddd
CodyWages <92700624+CodyWages@users.noreply.github.com>
2021-11-07 14:04:18 -0600

    Add files via upload

---
+++ ethgas.py
@@ -0,0 +1,15 @@
+#Import web scraping tools
+import requests
+import bs4
+
+#Store website in variable
+website = requests.get("https://www.coinmarketcap.com")
+
+#Store website data
+soup = bs4.BeautifulSoup(website.text,"lxml")
+
+#Store element data
+gas_var = soup.select("div>span>a")
+
+#Print current gwei
+print(f'Gas is Currently {gas_var[5].getText()}')
